e4027b84e297ae3e26c15380572890be65c61d2f
Jose Rios <jrbernabe@gmail.com>
2022-09-30 19:04:25 -0400

    Add cards to the search page and obtain title and description of movies from the TMDB API

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -9,11 +9,14 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { borderRadius } from "@mui/system";
 
 const Search = (props) => {
   const { search } = useParams();
 
   const [searchResult, setSearchResult] = useState([]);
+  const [casts, setCasts] = useState([]);
 
   useEffect(() => {
     axios
@@ -27,6 +30,29 @@ const Search = (props) => {
       .catch((response) => console.log(response));
   }, [search]);
 
+  const getCredits = (movieId) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
+      )
+      .then((castFound) => {
+        const newCasts = [...casts, castFound];
+        setCasts(newCasts);
+        console.log(casts);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getPrincipalActors = (movieId) => {
+    const movieCast = casts.filter((cast) => {
+      return cast.id === movieId;
+    });
+
+    return movieCast.filter((principalCast) => {
+      return principalCast < 3;
+    });
+  };
+
   return (
     <div className="search">
       <Sidebar />
@@ -34,26 +60,47 @@ const Search = (props) => {
       <div className="card-container">
         {searchResult.map((movie) => {
           const { id, title, overview, poster_path } = movie;
+          /* getCredits(id); */
           return (
-            <Card sx={{ maxWidth: 500, display: "flex" }} key={id}>
+            <Stack
+              direction="row"
+              backgroundColor="#2B303D"
+              width={{ xs: "90%", md: "75%", xl: "45%" }}
+              borderRadius="10px"
+              key={id}
+            >
               <CardMedia
                 component="img"
                 alt="green iguana"
-                height="140"
                 image={`http://image.tmdb.org/t/p/w500${poster_path}`}
+                sx={{ width: 200, borderRadius: "10px 0 0 10px" }}
               />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+              <Stack justifyContent="space-between" padding="1rem">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  color="#fff"
+                >
                   {title}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="#8b96a0">
                   {overview}
                 </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
+                <Typography variant="body2" color="#8b96a0">
+                  {title}
+                  {/* {console.log(getPrincipalActors(id))} */}
+                </Typography>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="custom"
+                  sx={{ width: "fit-content" }}
+                >
+                  Learn More
+                </Button>
+              </Stack>
+            </Stack>
           );
         })}
       </div>
