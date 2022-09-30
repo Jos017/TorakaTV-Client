bc4491b6fec28a30f783b401e9dbce527e2f3a7a
Jose Rios <jrbernabe@gmail.com>
2022-09-30 12:21:06 -0400

    Add avatar in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import "./Navbar.css";
 import * as PATHS from "../../utils/paths";
 import * as CONSTS from "../../utils/consts";
 import logo from "../../images/logo.png";
+import profileDefault from "../../images/profile-default.png";
 import SearchBar from "../SearchBar";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 
 const Navbar = (props) => {
   return (
@@ -19,14 +22,15 @@ const Navbar = (props) => {
       <div className="nav__authLinks">
         {/** Menu shown when a user is loged in */}
         {props.user ? (
-          <>
+          <Stack direction="row" spacing={2} alignItems="center">
             <Link to={PATHS.PROTECTEDPAGE} className="authLink">
               Protected Page
             </Link>
+            <Avatar src={profileDefault} alt="avatar" />
             <button className="nav-logoutbtn" onClick={props.handleLogout}>
               Logout
             </button>
-          </>
+          </Stack>
         ) : (
           <>
             <Link to="/auth/signup" className="authLink">
