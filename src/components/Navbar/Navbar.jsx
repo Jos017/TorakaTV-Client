494800ed9bfb395c6e628a2e284f16881da1353f
Jose Rios <jrbernabe@gmail.com>
2022-10-07 01:20:38 -0400

    Add menu to the user avatar in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import * as PATHS from "../../utils/paths";
 import logo from "../../images/logo.png";
 import profileDefault from "../../images/profile-default.png";
 import SearchBar from "../SearchBar";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuList from "@mui/material/MenuList";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
 
 const Navbar = (props) => {
+  const navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
     <nav>
       <Link to={"/"} className="nav__projectName">
@@ -25,13 +73,98 @@ const Navbar = (props) => {
         {/** Menu shown when a user is loged in */}
         {props.user ? (
           <Stack direction="row" spacing={2} alignItems="center">
-            <Link to={PATHS.PROTECTEDPAGE} className="authLink">
-              {/* Protected Page */}
-            </Link>
-            <Avatar
-              src={props.user.avatar ? props.user.avatar : profileDefault}
-              alt="avatar"
-            />
+            <Tooltip title="Account settings">
+              <IconButton
+                ref={anchorRef}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "composition-menu" : undefined}
+                aria-expanded={open ? "true" : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+              >
+                <Avatar
+                  src={props.user.avatar ? props.user.avatar : profileDefault}
+                  alt="avatar"
+                />
+              </IconButton>
+            </Tooltip>
+            <Popper
+              open={open}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              placement="bottom-start"
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === "bottom-start" ? "left top" : "left bottom",
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList
+                        autoFocusItem={open}
+                        id="composition-menu"
+                        aria-labelledby="composition-button"
+                        onKeyDown={handleListKeyDown}
+                      >
+                        <MenuItem
+                          onClick={(e) => {
+                            handleClose(e);
+                            navigate("/profile");
+                          }}
+                        >
+                          <ListItemIcon>
+                            <AccountCircleIcon />
+                          </ListItemIcon>
+                          Profile
+                        </MenuItem>
+                        <MenuItem
+                          onClick={(e) => {
+                            handleClose(e);
+                            navigate("/myList");
+                          }}
+                        >
+                          <ListItemIcon>
+                            <FactCheckIcon />
+                          </ListItemIcon>
+                          My List
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem
+                          onClick={(e) => {
+                            handleClose(e);
+                            navigate("/profile/edit");
+                          }}
+                        >
+                          <ListItemIcon>
+                            <Settings />
+                          </ListItemIcon>
+                          Edit Profile
+                        </MenuItem>
+                        <MenuItem>
+                          <ListItemIcon
+                            onClick={(e) => {
+                              handleClose(e);
+                              props.handleLogout();
+                            }}
+                          >
+                            <Logout />
+                          </ListItemIcon>
+                          Logout
+                        </MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+
             <button className="nav-logoutbtn" onClick={props.handleLogout}>
               Logout
             </button>
