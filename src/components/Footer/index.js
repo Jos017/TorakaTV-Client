2e342699496f28fe068a2c16ad6ab9d9fac1c2ec
Jose Rios <jrbernabe@gmail.com>
2022-11-11 17:25:07 -0400

    Add responsive to the Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,36 +1,41 @@
-import "./styles.css";
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Typography from "@mui/material/Typography";
+import './styles.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
-const Footer = (props) => {
+const Footer = props => {
   return (
     <footer>
-      <Stack
-        direction={{ xs: "column", sm: "row" }}
-        justifyContent="space-between"
-        width="70%"
-      >
-        <Link to="/" className="link">
-          Home
-        </Link>
-        <Link to="#" className="link">
-          About
-        </Link>
-        <Link to="/" className="link">
-          Movies
-        </Link>
-        <Link to="/" className="link">
-          TV Series
-        </Link>
+      <Grid container maxWidth="1000px" width="90%">
+        <Grid item xs={6} sm={2} textAlign="center">
+          <Link to="/" className="link">
+            Home
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={2} textAlign="center">
+          <Link to="#" className="link">
+            About
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={2} textAlign="center">
+          <Link to="/" className="link">
+            Movies
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={2} textAlign="center">
+          <Link to="/" className="link">
+            TV Series
+          </Link>
+        </Grid>
         {props.user ? (
-          <>
+          <Grid item xs={6} sm={2} textAlign="center">
             <Button
               variant="contained"
               color="error"
@@ -38,35 +43,44 @@ const Footer = (props) => {
             >
               Logout
             </Button>
-          </>
+          </Grid>
         ) : (
           <>
-            <Link to="/auth/signup" className="link">
-              Signup
-            </Link>
-            <Link to="/auth/login" className="link">
-              Log In
-            </Link>
+            <Grid item xs={6} sm={2} textAlign="center">
+              <Link to="/auth/signup" className="link">
+                Signup
+              </Link>
+            </Grid>
+            <Grid item xs={6} sm={2} textAlign="center">
+              <Link to="/auth/login" className="link">
+                Log In
+              </Link>
+            </Grid>
           </>
         )}
-      </Stack>
+      </Grid>
+      <hr className="footer-divider" />
       <Stack
-        direction={{ xs: "column", sm: "row" }}
+        direction={{ xs: 'column', sm: 'row' }}
         justifyContent="space-between"
-        width="70%"
+        maxWidth="1000px"
+        width="95%"
+        textAlign="center"
       >
-        <Stack direction="row" justifyContent="flex-end">
-          <Typography variant="body1">
+        <Stack
+          direction="row"
+          justifyContent={{ xs: 'center', sm: 'flex-end' }}
+        >
+          <Typography variant="body1" margin="0.5rem 0">
             TorakaTV ©2022 All Rights Reserved.
           </Typography>
         </Stack>
         <Stack
           direction="row"
           alignItems="center"
-          justifyContent={{ xs: "flex-end" }}
+          justifyContent={{ xs: 'center', sm: 'flex-end' }}
           spacing={2}
         >
-          <Typography variant="body1">Follow Us</Typography>
           <FacebookIcon fontSize="large" />
           <TwitterIcon fontSize="large" />
           <InstagramIcon fontSize="large" />
