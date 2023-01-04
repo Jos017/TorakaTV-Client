7978d0e1f6ffb172e8c92927afb320feb6d840a3
Jose Rios <jrbernabe@gmail.com>
2023-01-04 15:26:28 -0400

    Fix responsive issues in My Profile Page

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -65,10 +65,15 @@ const ProfilePage = (props) => {
         direction="row"
         alignItems="center"
         spacing={3}
-        marginTop={{ xs: 12, sm: 1 }}
+        marginTop={{ xs: 10, sm: 1 }}
         marginBottom={3}
       >
-        <Typography variant="h2" color="#fff" fontWeight="Bold">
+        <Typography
+          variant="h2"
+          color="#fff"
+          fontWeight="Bold"
+          fontSize={{ xs: '2rem', md: '3rem' }}
+        >
           My Profile
         </Typography>
         <Button
@@ -86,46 +91,59 @@ const ProfilePage = (props) => {
         <Paper
           elevation={3}
           sx={{
-            backgroundColor: "#4e4f50",
-            borderRadius: "1rem",
-            padding: "2rem",
+            backgroundColor: '#4e4f50',
+            borderRadius: '1rem',
+            padding: {xs: '1.6rem', sm: '2rem'},
             flexGrow: 1,
-            maxWidth: "60rem",
-            margin: "0 auto",
+            maxWidth: '60rem',
+            margin: '0 auto',
           }}
         >
           <Grid container borderRadius={3} spacing={1}>
-            <Grid item xs="auto" sx={{ margin: "0 auto" }}>
-              <Paper className="profile-img-container" elevation={3}>
-                <img src={avatar ? avatar : profilePic} alt="profile" />
+            <Grid item alignContent='center' xs={12} sm="auto" sx={{ margin: '0 auto' }}>
+              <Paper
+                elevation={3}
+                sx={{
+                  borderRadius: { xs: '50%', sm: '1rem', md: '1rem' },
+                  width: { xs: '155px', sm: '200px', md: '300px' },
+                  height: { xs: '155px', sm: '200px', md: '300px' },
+                  overflow: 'hidden',
+                  margin: '0 auto',
+                }}
+              >
+                <img
+                  className="profile-img"
+                  src={avatar ? avatar : profilePic}
+                  alt="profile"
+                />
               </Paper>
             </Grid>
             <Grid item xs>
               <Paper
                 elevation={3}
                 sx={{
-                  backgroundColor: "#242526",
-                  borderRadius: "1rem",
-                  padding: "2rem",
-                  height: "100%",
+                  backgroundColor: '#242526',
+                  borderRadius: '1rem',
+                  padding: {xs: '1.6rem', sm: '2rem'},
+                  height: '100%',
                 }}
               >
                 <Typography variant="h5" color="#fff">
                   {firstName} {lastName}
                 </Typography>
-                <Typography variant="subtitle1" component="div" color="#4e4f50">
+                <Typography variant="subtitle1" component="div" color="#bbb">
                   @{username}
                 </Typography>
-                <Typography variant="body1" component="div" color="#fff">
-                  <span style={{ color: "#13c6b2" }}>Email: </span>
+                <Typography variant="body1" component="div" color="#bbb">
+                  <span style={{ color: '#13c6b2' }}>Email: </span>
                   <span>{email}</span>
                 </Typography>
-                <Typography variant="body1" component="div" color="#fff">
-                  <span style={{ color: "#13c6b2" }}>Phone Number: </span>
+                <Typography variant="body1" component="div" color="#bbb">
+                  <span style={{ color: '#13c6b2' }}>Phone: </span>
                   <span>{phone}</span>
                 </Typography>
-                <Typography variant="body1" component="div" color="#fff">
-                  <span style={{ color: "#13c6b2" }}>About me: </span>
+                <Typography variant="body1" component="div" color="#bbb">
+                  <span style={{ color: '#13c6b2' }}>About me: </span>
                   <span>{about}</span>
                 </Typography>
               </Paper>
@@ -135,43 +153,81 @@ const ProfilePage = (props) => {
                 elevation={3}
                 className="comments-section"
                 sx={{
-                  backgroundColor: "#242526",
-                  borderRadius: "1rem",
-                  padding: "2rem",
-                  marginTop: "1rem",
+                  backgroundColor: '#242526',
+                  borderRadius: '1rem',
+                  padding: '2rem',
+                  marginTop: '1rem',
                   flexGrow: 1,
                 }}
               >
-                <Stack direction="row" alignItems="center" spacing={1}>
-                  <LiveTvIcon color="custom" fontSize="large" />
-                  <Stack>
-                    <Typography variant="body1" color="#fff" component="div">
-                      {userSession?.list.length} Items
+                <Stack
+                  width={1}
+                  direction="row"
+                  justifyContent="space-evenly"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Stack alignItems="center" width={{xs: '50px', sm: '100px', md: '150px'}}>
+                    <LiveTvIcon color="custom" fontSize="large" />
+                    <Typography
+                      variant="body1"
+                      color="#fff"
+                      component="div"
+                      textAlign="center"
+                      display={{ xs: 'none', sm: 'inline' }}
+                    >
+                      Series and Movies
                     </Typography>
-                    <Typography variant="body1" color="#fff" component="div">
-                      Total Series and Movies Tracked
+                    <Typography
+                      variant="body1"
+                      color="#13c6b2"
+                      component="div"
+                      textAlign="center"
+                      fontSize="1.6rem"
+                    >
+                      {userSession?.list.length}
                     </Typography>
                   </Stack>
-                </Stack>
-                <Stack direction="row" alignItems="center" spacing={1}>
-                  <CommentIcon color="error" fontSize="large" />
-                  <Stack>
-                    <Typography variant="body1" color="#fff" component="div">
-                      {userSession?.comments.length} Comments
+                  <Stack alignItems="center" width={{xs: '50px', sm: '100px', md: '150px'}}>
+                    <CommentIcon color="error" fontSize="large" />
+                    <Typography
+                      variant="body1"
+                      color="#fff"
+                      component="div"
+                      textAlign="center"
+                      display={{ xs: 'none', sm: 'inline' }}
+                    >
+                      Comments
                     </Typography>
-                    <Typography variant="body1" color="#fff" component="div">
-                      Total Comments made
+                    <Typography
+                      variant="body1"
+                      color="#D32F2F"
+                      component="div"
+                      textAlign="center"
+                      fontSize="1.6rem"
+                    >
+                      {userSession?.comments.length}
                     </Typography>
                   </Stack>
-                </Stack>
-                <Stack direction="row" alignItems="center" spacing={1}>
-                  <StarIcon color="warning" fontSize="large" />
-                  <Stack>
-                    <Typography variant="body1" color="#fff" component="div">
-                      {userSession?.ranking.length} Ratings
+                  <Stack alignItems="center" width={{xs: '50px', sm: '100px', md: '150px'}}>
+                    <StarIcon color="warning" fontSize="large" />
+                    <Typography
+                      variant="body1"
+                      color="#fff"
+                      component="div"
+                      textAlign="center"
+                      display={{ xs: 'none', sm: 'inline' }}
+                    >
+                      Ratings
                     </Typography>
-                    <Typography variant="body1" color="#fff" component="div">
-                      Total Series and Movies Rated
+                    <Typography
+                      variant="body1"
+                      color="#ED6C02"
+                      component="div"
+                      textAlign="center"
+                      fontSize="1.6rem"
+                    >
+                      {userSession?.ranking.length}
                     </Typography>
                   </Stack>
                 </Stack>
